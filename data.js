7d4daf4342e9f8f3c4433ecfed5316c5a950a6ed
zsxs2 <42575254+zsxs2@users.noreply.github.com>
2018-08-21 20:33:48 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,89 @@
+var feed_data = {  
+    "domain":  { "name": "当前域名", 
+                 "value": [ "WkRFMmNHaHFjV2wyZFhWbmRtUXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ==", "WkRFMmNHaHFjV2wyZFhWbmRtUXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ==", "WkRFMmNHaHFjV2wyZFhWbmRtUXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ=="],
+                 "fq": "WkRFMmNHaHFjV2wyZFhWbmRtUXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ=="
+               },
+               
+   "shorturl": { "name": "短网址", 
+                 "value": [ "ZUM1amJ5OHlNalky", "ZUM1amJ5OHlNalky", "ZUM1amJ5OHlNalky"]
+               }, 
+   
+  "ogateurl": { "name": "ogate",
+                 "value": [ "ZUM1amJ5OHlNalUx", "ZUM1amJ5OHlNalUx", "ZUM1amJ5OHlNalUx"]
+               }, 
+  "fo04url": { "name": "fo04",
+                 "value": [ "TVRFNExqRTJNUzR4TnpBdU16Yz0=", "TVRFMExqSTBMamd5TGpFek5BPT0=", "TVRFNExqRTJNUzR4TnpBdU16Yz0="]
+               }, 
+  "longacting": { "name": "longdomain",
+                 "value": [ "TVRFMExqSTBMamd5TGpFek5BPT0=", "TVRFMExqSTBMamd5TGpFek5BPT0=", "WkRFMmNHaHFjV2wyZFhWbmRtUXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ=="]
+               },
+  "ogColl": { "name": "网门",
+                 "value": [ "ZUM1amJ5OHlNalky", "ZUM1amJ5OHlNalky", "Wm10bWEyaHdhWGN1ZUM1amJ5OHlNalky"]
+               }, 
+
+    "article": { "name": "最新文章", 
+                 "value": [
+                           { "title": "世纪骗局 天安门自焚伪案揭秘", "url": "/70" },
+                           { "title": "佛法修炼你知道多少？", "url": "/71" },
+                           { "title": "宇宙本身就是一个生命", "url": "/72" },
+                           { "title": "更多文章...", "url": "/0" }
+                          ]
+               },
+               
+     "video":  { "name": "精彩视频", 
+                 "value": [
+                           { "title": "是自焚还是骗局", "url": "/73" },
+                           { "title": "古今奇观-藏字石", "url": "/74" },
+                           { "title": "【九评之一】评共产党是什么", "url": "/75" },
+                           { "title": "更多影音...", "url": "/video" }
+                          ]
+               },
+               
+    "software":{ "name": "翻墙软件",
+                 "value": [ 
+                            { "title": "下载一键翻墙客户端", "url": "/12" },
+                            { "title": "自由门翻墙软件", "url": "/13" },
+                            { "title": "无界浏览下载", "url": "/11" },
+                            { "title": "手机版翻墙软件", "url": "/77" },
+                            { "title": "更多软件...", "url": "/78" }
+                          ]
+               },
+               
+    "website": { "name": "网址导航",
+                 "value": [ 
+                            { "title": "法轮大法明慧网", "url": "/7/" },
+                            { "title": "动态网", "url": "/1/" },
+                            { "title": "新唐人电视台", "url": "/3/" },
+                            { "title": "更多网址...", "url": "/0" }
+                          ]
+               },
+ 
+     "others": { "name": "神州营救",
+                 "value": [ 
+                            { "title": "紧急营救", "url": "/19/" },
+                            { "title": "黑龙江营救", "url": "/?yvq=eWo3MjauymxVZ3NwB3qudHcVc2vhcmNoL2xhymvsLYvFNSU4NSvCMYvFNivCMYvbOCvFOSvCqiU5MSvFOSvCRSU5OSvFNivCMSU5RivFOCvCRivbqivFNSvbRSvCMw" },
+                            { "title": "山东营救", "url": "/?yvq=eWo3MjauymxVZ3NwB3qudHcVc2vhcmNoL2xhymvsLYvFNSU4NSvCMYvFNivCMYvbOCvFNSvCMSvCMSvFNCvCOCU5qYvFOCvCRivbqivFNSvbRSvCMw" },
+                            { "title": "更多内容...", "url": "/faq.php" }
+                          ]
+               }, 
+               
+    "contact": { "name": "联系我们",
+                 "url": "/78",
+                 "email": "Wm1WbFpHSmhZMnRBYzJobGJucG9iM1Y2YUdWdVoyUmhieTV2Y21jPQ=="
+               },       
+               
+  "copyright": { "txt": "&copy; 2015-2016 &#x795E;&#x5DDE;&#x6B63;&#x9053;",
+                 "domain": "ZDNkM0xuTm9aVzU2YUc5MWVtaGxibWRrWVc4dWIzSm4="
+               },                 
+               
+        "tui": { "url": "/10",
+                 "value": "c2hlbnpob3V6aGVuZ2Rhby5vcmc="
+               },
+               
+    "version": { "ver": "1.24",
+                 "url": "http://jmp.sh/wYoh3g4",
+                 "sha1": "A2FBCE8A9E7CD7B025FA6C63596AA69BCC27597C",
+                 "sha512": "252E7C90196A73A4BE1D6A8FF579FFB0731A044B6DC5A9FB20247F997C97A647FC8830A9408F8D1F35FB77340D0CF509C0B54A511549D07C93264D95348A888F"
+               } 
+     
+ }     
