757c3cbc561466102728a451460414f48e05ceeb
zsxs2 <42575254+zsxs2@users.noreply.github.com>
2020-08-11 06:28:20 +0000

    Update data.js

--- data.js
+++ data.js
@@ -1,75 +1,71 @@
-var feed_data = {  
-    "domain":  { "name": "当前域名", 
-                 "value": [ "WkdkbmIyTnJiMnN6YVc1MllTNWpiRzkxWkdaeWIyNTBMbTVsZEE9PQ==", "WkhaM05tZHVaVGQwWXpBNWR5NWpiRzkxWkdaeWIyNTBMbTVsZEE9PQ==", "WkRkemF6UXliSEpqT0dWd09DNWpiRzkxWkdaeWIyNTBMbTVsZEE9PQ==","WkROa2EyTnZhbTVzZDNJeGVURXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ==", "WkRNd05HcDBaSEZ2WkhNeGR6a3VZMnh2ZFdSbWNtOXVkQzV1WlhRPQ==", "Wkd0ME1YbHFialZ2ZGpKdmN5NWpiRzkxWkdaeWIyNTBMbTVsZEE9PQ=="],
-                 "fq": "WkRKdWEybDVhakJ0TkRaMFlYUXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ=="
+var feed_data =
+{"domain":{"name":"\u5f53\u524d\u57df\u540d",
+                 "value": [ "WkdkbmIyTnJiMnN6YVc1MllTNWpiRzkxWkdaeWIyNTBMbTVsZEE9PQ==", "WkhaM05tZHVaVGQwWXpBNWR5NWpiRzkxWkdaeWIyNTBMbTVsZEE9PQ==", "WkRkemF6UXliSEpqT0dWd09DNWpiRzkxWkdaeWIyNTBMbTVsZEE9PQ=="],
+                 "fq": "WkRNMlpUbGtOMjF2T1dKaE4yTXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ=="
                },
                
-   "shorturl": { "name": "短网址", 
-                 "value": [ "ZEM1amJpOUJOa3gxYm1kaFZRPT0=", "ZEM1amJpOUJOa3gxYmtScFlRPT0=", "ZEM1amJpOUJOa3gxYmtScFlRPT0=","ZEM1amJpOUJOa3gxYm1kaFZRPT0=", "ZEM1amJpOUJOa3gxYmtScFlRPT0=", "ZEM1amJpOUJOa3gxYm1kaFZRPT0="]
+"shorturl":{"name":"\u77ed\u7f51\u5740",
+                 "value": [ "ZEM1amJpOUJOa3gxYm1kaFZRPT0=", "ZEM1amJpOUJOa3gxYmtScFlRPT0=", "ZEM1amJpOUJOa3gxYm1kaFZRPT0="]
                }, 
    
   "ogateurl": { "name": "ogate",
-                 "value": [ "ZEM1amJpOUJOa3gxYm1Wb2N3PT0=", "ZEM1amJpOUJOa3gxYm1Wb2N3PT0=", "ZEM1amJpOUJOa3gxYm1Wb2N3PT0=","ZEM1amJpOUJOa3gxYm1Wb2N3PT0=", "ZEM1amJpOUJOa3gxYm1Wb2N3PT0=", "ZEM1amJpOUJOa3gxYm1Wb2N3PT0="]
+                 "value": [ "ZEM1amJpOUJOa3gxYm1Wb2N3PT0=", "ZEM1amJpOUJOa3gxYm1Wb2N3PT0=", "ZEM1amJpOUJOa3gxYm1Wb2N3PT0="]
                }, 
   "fo04url": { "name": "fo04",
                  "value": [ "TVRFeExqSTBNUzQ0Tnk0eE1EUT0=", "TVRFNExqRTJOeTR4TURRdU1UTXc=", "TmpFdU1qSTRMakU0TUM0NU1nPT0="]
                }, 
   "longacting": { "name": "longdomain",
-                 "value": [ "TlRJdU1UazJMakkwT1M0NU13PT0=", "TWpFNUxqZzFMams0TGpFNQ==", "YzNocVluRm9MbVF4ZFdocWRYTTVZekp5ZWpjM0xtTnNiM1ZrWm5KdmJuUXVibVYw","WW1WaVlYY3VjM3A2WkM1NGVYbz0=", "YW5KMWRuZHFMbmxwYm1kMWJ5NXphWFJs", "TmpFdU1qSTRMakl4Tnk0eE56QT0="]
+                 "value": [ "TlRJdU1UazJMakkwT1M0NU13PT0=", "TWpFNUxqZzFMams0TGpFMQ==", "ZEdSc2FHRXVaREYxYUdwMWN6bGpNbko2TnpjdVkyeHZkV1JtY205dWRDNXVaWFE9"]
                },
-  "ogColl": { "name": "网门",
-                 "value": [ "ZEM1amJpOUZlbkZxTmtWVg==", "ZEM1amJpOUZSMWhyYlVWbQ==", "ZUhsemFtOXZjUzV1YldWNWFuRnRkUzV0YkM4Mk9BPT0="]
+"ogColl":{"name":"\u7f51\u95e8",
+                 "value": [ "ZEM1amJpOUZlbkZxV2xwdQ==", "ZEM1amJpOUZSMWhyTXpObg==", "ZUhwNVoyNXRMbTV0Wld4MWJXd3VaMkV2TmpnPQ=="]
                }, 
-
-               
-   "video":  { "name": "精彩视频",
-                 "value": [
-                           { "title": "新唐人直播", "url": "/tEXS"},
-                           { "title": "新唐人亚太台", "url": "/v.php?id=ntd-mlt"},
-                           { "title": "天国乐团", "url": "/video/play/1094.html" },
-                           { "title": "共产主义的终极目的", "url": "/video/play/1035.html" },                   
-                           { "title": "更多影音...", "url": "/video/" }
-                          ]
-               },
-               
-    "software":{ "name": "翻墙软件",
-                 "value": [ 
-                           { "title": "神州明见安卓版一键翻墙客户端", "url": "/FYMXJ" },
-                           { "title": "自由门安卓版", "url": "/5c5tX" },
-                           { "title": "安卓版无界一点通", "url": "/Wf89M" },
-                           { "title": "神州明见TV安卓版", "url": "/CLZDc" },
-                           { "title": "更多软件...", "url": "https://git.io/szzd" }
-                          ]
-               },
-               
-    "website": { "name": "网址导航",
-                 "value": [ 
-                            { "title": "法轮大法明慧网", "url": "/7/" },
-                            { "title": "动态网", "url": "/1/" },
-                            { "title": "新唐人电视台", "url": "/3/" },
-                            { "title": "更多网址...", "url": "/0" }
-                          ]
-               },
- 
-               
-    "contact": { "name": "联系我们",
-                 "url": "/78",
-                 "email": "YzJobGJucG9iM1Y2YUdWdVoyUmhiMEJuYldGcGJDNWpiMjA9"
-               },       
-               
-  "copyright": { "txt": "&copy; 2015-2020 &#x795E;&#x5DDE;&#x6B63;&#x9053;",
-                 "domain": "ZDNkM0xuTm9aVzU2YUc5MWVtaGxibWRrWVc4dWIzSm4="
-               },                 
-               
-        "tui": { "url": "/10",
-                 "value": "c2hlbnpob3V6aGVuZ2Rhby5vcmc="
-               },
-               
-"version": { "ver": "5.0",
-                "url": "https://git.io/fxNjC",
-                 "sha1": "555ACDC8F509DC216E45DC8EAA064F3A900A83F0",
-                 "sha512": "F06EDF7DDFB483AF55F6775FEB3C41D345923F5C0E8CE201944BC530321859BEDCC3DF7A96B98D568918D6F8E6310C5955D56DCA90FE8669DD143F34E8A7FB34",
-               "time": "1597126427"
-               } 
-     
- }     
+"article":{"name":"\u6700\u65b0\u6587\u7ae0",
+"value":[]},
+"video":{"name":"\u7cbe\u5f69\u89c6\u9891",
+"value":[{"title":"\u65b0\u5510\u4eba\u76f4\u64ad",
+"url":"\/tEXS?ryxds"},
+{"title":"\u65b0\u5510\u4eba\u4e9a\u592a\u53f0",
+"url":"\/v.php?id=ntd-mlt"},
+{"title":"\u5929\u56fd\u4e50\u56e2",
+"url":"\/video\/play\/1094.html"},
+{"title":"\u5171\u4ea7\u4e3b\u4e49\u7684\u7ec8\u6781\u76ee\u7684",
+"url":"\/video\/play\/1035.html"},
+{"title":"\u66f4\u591a\u5f71\u97f3...",
+"url":"\/video\/"}]},
+"software":{"name":"\u7ffb\u5899\u8f6f\u4ef6",
+"value":[{"title":"\u795e\u5dde\u660e\u89c1\u5b89\u5353\u7248\u4e00\u952e\u7ffb\u5899\u5ba2\u6237\u7aef",
+"url":"\/FYMXJ?lenva"},
+{"title":"\u81ea\u7531\u95e8\u5b89\u5353\u7248",
+"url":"\/5c5tX?yyzvy"},
+{"title":"\u5b89\u5353\u7248\u65e0\u754c\u4e00\u70b9\u901a",
+"url":"\/Wf89M?afkoh"},
+{"title":"\u795e\u5dde\u660e\u89c1TV\u5b89\u5353\u7248",
+"url":"\/CLZDc?mohin"},
+{"title":"\u66f4\u591a\u8f6f\u4ef6...",
+"url":"https:\/\/git.io\/szzd"}]},
+"website":{"name":"\u7f51\u5740\u5bfc\u822a",
+"value":[{"title":"\u6cd5\u8f6e\u5927\u6cd5\u660e\u6167\u7f51",
+"url":"\/GnoF?lqvnn"},
+{"title":"\u52a8\u6001\u7f51",
+"url":"\/dCyE?ewnzc"},
+{"title":"\u65b0\u5510\u4eba\u7535\u89c6\u53f0",
+"url":"\/V8cz?vhmdv"},
+{"title":"\u66f4\u591a\u7f51\u5740...",
+"url":"\/TRG2?zuvtp"}]},
+"others":{"name":"\u795e\u5dde\u8425\u6551",
+"value":[]},
+"contact":{"name":"\u8054\u7cfb\u6211\u4eec",
+"url":"https:\/\/git.io\/szzd",
+"email":"YzJobGJucG9iM1Y2YUdWdVoyUmhiMEJuYldGcGJDNWpiMjA9"},
+"copyright":{"txt":"&copy; 2015-2020 \u795e\u5dde\u6b63\u9053",
+"domain":"ZDNkM0xuTm9aVzU2YUc5MWVtaGxibWRrWVc4dWIzSm4="},
+"tui":{"url":"\/QUdH8?syspl",
+"value":"c2hlbnpob3V6aGVuZ2Rhby5vcmc="},
+"version":{"ver":"5.0",
+"url":"https:\/\/git.io\/fxNjC",
+"sha1":"555ACDC8F509DC216E45DC8EAA064F3A900A83F0",
+"sha512":"F06EDF7DDFB483AF55F6775FEB3C41D345923F5C0E8CE201944BC530321859BEDCC3DF7A96B98D568918D6F8E6310C5955D56DCA90FE8669DD143F34E8A7FB34",
+"time":"1597127301"
+}
+}
